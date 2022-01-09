12bbce6197ca90ff8e3ac9ed9fe5d57ad192be4a
Andrea Francia <andrea@andreafrancia.it>
2022-01-09 13:34:15 +0100

    First commit

---
+++ tests/StringCalculatorTest.php
@@ -0,0 +1,11 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class StringCalculatorTest extends TestCase
+{
+    function test() {
+        $calculator = new StringCalculator();
+
+        self::assertEquals(1,1);
+    }
+}
